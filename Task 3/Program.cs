f55e009de27ba486b78499ff237b1963c0c568cb
Tatiana Kvon <kvn-tn@mail.ru>
2022-11-21 19:13:04 +0300

    task3

---
+++ Task 3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 
+// 7 и 23.
+
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+int num1 = new Random().Next(10,1000);
+
+Console.WriteLine(num1);
+
+if((num1%7==0)&&(num1%23==0))
+
+//  && - и
+// || - или
+
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+// int num2 = new Random().Next(10,1000);
+
+// Console.WriteLine(num1);
+
+// if((num1%7==0)&&(num1%23==0))
+
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
